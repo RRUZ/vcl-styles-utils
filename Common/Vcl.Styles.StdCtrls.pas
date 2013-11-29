62f03a0c7b6825d3f3c3bb96e76651c70c5d33b1
Rodrigo.Ruz.V@gmail.com <Rodrigo.Ruz.V@gmail.com@b5adaa1f-121e-a19e-4a49-83ea36b37e0a>
2013-11-29 18:54:19 +0000

    Added support for TNewNotebook (Inno Setup component) Added support for TNewNotebookPage (Inno Setup component) Added support for TCheckBox component

--- Common/Vcl.Styles.StdCtrls.pas
+++ Common/Vcl.Styles.StdCtrls.pas
@@ -41,6 +41,18 @@ type
     constructor Create(AHandle: THandle); override;
   end;
 
+  TCheckBoxState = (cbUnchecked, cbChecked, cbGrayed);
+  TCheckBoxTextWnd = class(TControlWnd)
+  private
+    FPressed : Boolean;
+    function GetDrawState(State: TCheckBoxState): TThemedButton;
+  protected
+    procedure WndProc(var Message: TMessage); override;
+  public
+    constructor Create(AHandle: THandle); override;
+  end;
+
+
 implementation
 
 uses
@@ -97,7 +109,7 @@ const
 
 Borders: array[TStaticBorderStyle] of DWORD = (0, WS_BORDER, SS_SUNKEN);
 var
-  uMsg: UINT;
+  Msg: UINT;
   DC: HDC;
   LDetails: TThemedElementDetails;
   LCanvas : TCanvas;
@@ -106,8 +118,8 @@ var
   LBuffer: TBitmap;
   C1, C2: TColor;
 begin
-  uMsg := Message.Msg;
-  case uMsg of
+  Msg := Message.Msg;
+  case Msg of
     WM_NCPAINT:
       begin
           LCanvas := TCanvas.Create;
@@ -175,6 +187,158 @@ begin
            DrawControlText(LCanvas, LDetails, Text, LRect, DT_RIGHT or DT_WORDBREAK)
           else
            DrawControlText(LCanvas, LDetails, Text, LRect, DT_LEFT or DT_WORDBREAK);
+
+          if DC = 0 then
+            EndPaint(Handle, lpPaint);
+        finally
+          LCanvas.Handle := 0;
+          LCanvas.Free;
+        end;
+      end;
+
+  else
+    Message.Result := CallOrgWndProc(Message);
+  end;
+end;
+
+{ TCheckBoxTextWnd }
+
+constructor TCheckBoxTextWnd.Create(AHandle: THandle);
+begin
+  inherited;
+  FPressed:=False;
+end;
+
+function TCheckBoxTextWnd.GetDrawState(State: TCheckBoxState): TThemedButton;
+begin
+  Result := tbButtonDontCare;
+
+  if not Enabled then
+    case State of
+      cbUnChecked: Result := tbCheckBoxUncheckedDisabled;
+      cbChecked: Result := tbCheckBoxCheckedDisabled;
+      cbGrayed: Result := tbCheckBoxMixedDisabled;
+    end
+  else if FPressed and MouseInControl then
+    case State of
+      cbUnChecked: Result := tbCheckBoxUncheckedPressed;
+      cbChecked: Result := tbCheckBoxCheckedPressed;
+      cbGrayed: Result := tbCheckBoxMixedPressed;
+    end
+  else if MouseInControl then
+    case State of
+      cbUnChecked: Result := tbCheckBoxUncheckedHot;
+      cbChecked: Result := tbCheckBoxCheckedHot;
+      cbGrayed: Result := tbCheckBoxMixedHot;
+    end
+  else
+    case State of
+      cbUnChecked: Result := tbCheckBoxUncheckedNormal;
+      cbChecked: Result := tbCheckBoxCheckedNormal;
+      cbGrayed: Result := tbCheckBoxMixedNormal;
+    end;
+end;
+
+procedure TCheckBoxTextWnd.WndProc(var Message: TMessage);
+var
+  Msg: UINT;
+  DC: HDC;
+  LDetails: TThemedElementDetails;
+  LCanvas : TCanvas;
+  LRect: TRect;
+  lpPaint: TPaintStruct;
+  State: TCheckBoxState;
+  Spacing : Integer;
+  BoxSize : TPoint;
+  LCaption: string;
+begin
+  Msg := Message.Msg;
+  case Msg of
+
+     WM_LBUTTONUP,
+     WM_LBUTTONDOWN : begin
+                        SetRedraw(False);
+                        CallOrgWndProc(Message);
+                        SetRedraw(True);
+                        FPressed := (Msg=WM_LBUTTONDOWN);
+                        Invalidate;
+                      end;
+    BM_SETCHECK,
+    WM_LBUTTONDBLCLK :begin
+                        SetRedraw(False);
+                        CallOrgWndProc(Message);
+                        SetRedraw(True);
+                        Invalidate;
+                      end;
+
+    WM_KEYUP,
+    WM_KEYDOWN       :begin
+                          if TWMKeyDown(Message).CharCode = VK_SPACE then
+                            SetRedraw(False);
+                          CallOrgWndProc(Message);
+                          if TWMKeyDown(Message).CharCode = VK_SPACE then
+                          begin
+                            SetRedraw(True);
+                            Invalidate;
+                          end;
+                      end;
+
+    WM_PAINT:
+      begin
+        DC := HDC(Message.WParam);
+        LCanvas := TCanvas.Create;
+        try
+          if DC <> 0 then
+            LCanvas.Handle := DC
+          else
+            LCanvas.Handle := BeginPaint(Handle, lpPaint);
+
+          //background
+          LDetails.Element := teButton;
+          if StyleServices.HasTransparentParts(LDetails) then
+              StyleServices.DrawParentBackground(Handle, LCanvas.Handle, LDetails, False);
+
+          //paint
+          State := TCheckBoxState(SendMessage(Handle, BM_GETCHECK, 0, 0));
+          LDetails := StyleServices.GetElementDetails(GetDrawState(State));
+
+          Spacing := 5;
+          LRect := ClientRect;
+          BoxSize := Point(13, 13);
+          if not (Style and BS_RIGHTBUTTON = BS_RIGHTBUTTON) then
+          begin
+            LRect := Rect(0, 0, BoxSize.X, BoxSize.Y);
+            RectVCenter(LRect, Rect(0, 0, Width, Height));
+          end
+          else
+          begin
+            LRect := Rect(Width - BoxSize.X - 1, 0, Width, Height);
+            RectVCenter(LRect, Rect(Width - BoxSize.X - 1, 0, Width, Height));
+          end;
+
+          StyleServices.DrawElement(LCanvas.Handle, LDetails, LRect);
+          LCanvas.Font := Self.Font;
+
+          LRect := Rect(0, 0, Width - BoxSize.X - 10, Height);
+          LCaption := Text;
+          DrawText(LCanvas.Handle, PWideChar(LCaption), Length(LCaption), LRect, (DT_CALCRECT or DT_EXPANDTABS));
+
+          if not (Style and BS_RIGHTBUTTON = BS_RIGHTBUTTON) then
+            RectVCenter(LRect, Rect(BoxSize.X + Spacing, 0, Width, Height));
+
+          DrawControlText(LCanvas, LDetails, LCaption, LRect, (DT_LEFT or DT_VCENTER or DT_EXPANDTABS));
+
+          if Focused then
+          begin
+            InflateRect(LRect, 2, 1);
+            if LRect.Top < 0 then
+              LRect.Top := 0;
+            if LRect.Bottom > Height then
+              LRect.Bottom := Height;
+            LCanvas.Brush.Color := StyleServices.GetSystemColor(clBtnFace);
+            LCanvas.DrawFocusRect(LRect);
+          end;
+
 
           if DC = 0 then
             EndPaint(Handle, lpPaint);
