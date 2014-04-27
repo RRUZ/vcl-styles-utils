e47990d5ce29c6999b1a84df82d050436e32883b
Rodrigo.Ruz.V@gmail.com <Rodrigo.Ruz.V@gmail.com@b5adaa1f-121e-a19e-4a49-83ea36b37e0a>
2014-04-27 01:39:14 +0000

    Removed  Vcl.Styles.Ext warnings Added detours library Added to Vcl.Styles.Hooks support for 64 bita Added New demo App

---
+++ Vcl Styles Utils Addtional Dialogs (Demo App)/Dialogs2.dpr
@@ -0,0 +1,25 @@
+program Dialogs2;
+
+uses
+  Vcl.Forms,
+  uMain in 'uMain.pas' {FrmMain},
+  Vcl.Themes,
+  Vcl.Styles,
+  Vcl.Styles.Utils.ComCtrls in '..\Common\Vcl.Styles.Utils.ComCtrls.pas',
+  Vcl.Styles.Utils.Forms in '..\Common\Vcl.Styles.Utils.Forms.pas',
+  Vcl.Styles.Utils.Menus in '..\Common\Vcl.Styles.Utils.Menus.pas',
+  Vcl.Styles.Utils.ScreenTips in '..\Common\Vcl.Styles.Utils.ScreenTips.pas',
+  Vcl.Styles.Utils.StdCtrls in '..\Common\Vcl.Styles.Utils.StdCtrls.pas',
+  Vcl.Styles.Utils.SysControls in '..\Common\Vcl.Styles.Utils.SysControls.pas',
+  Vcl.Styles.Utils.SysStyleHook in '..\Common\Vcl.Styles.Utils.SysStyleHook.pas',
+  ObjSel in 'ObjSel.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  TStyleManager.TrySetStyle('Carbon');
+  Application.CreateForm(TFrmMain, FrmMain);
+  Application.Run;
+end.
