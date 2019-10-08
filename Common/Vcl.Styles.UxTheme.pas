686fc2cc9757f849ee3350a61ed1f8acf9479339
Daniel-Tr <trierweiler@jam-software.com>
2019-10-08 10:32:28 +0200

    * merged conflicting change from upstream/master:
    Fix for wrong background color in open file dialog on Windows 7 made by rickard67
    https://github.com/RRUZ/vcl-styles-utils/issues/256

--- Common/Vcl.Styles.UxTheme.pas
+++ Common/Vcl.Styles.UxTheme.pas
@@ -690,6 +690,27 @@ begin
     end
     else
     {$ENDIF}
+    {$IFDEF HOOK_Menu}
+    if SameText(LThemeClass, 'MenuStyle') then
+    begin
+      pColor := clNone;
+      case iPartId of
+        13:
+          case iStateId  of
+            0 : pColor := ColorToRGB(StyleServices.GetSystemColor(clBtnFace));
+          end;
+      end;
+
+      if TColor(pColor) = clNone then
+      begin
+        Result := Trampoline_UxTheme_GetThemeColor(hTheme, iPartId, iStateId, iPropId, pColor);
+        //OutputDebugString(PChar(Format('Detour_GetThemeColor Class %s hTheme %d iPartId %d iStateId %d  iPropId %d Color %8.x', [LThemeClass, hTheme, iPartId, iStateId, iPropId, pColor])));
+      end
+      else
+        Result := S_OK;
+    end
+    else
+    {$ENDIF}
     if SameText(LThemeClass, VSCLASS_TEXTSTYLE) then
     begin
       pColor := clNone;
