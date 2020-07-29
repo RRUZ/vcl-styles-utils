ec1d56c97f67d7608efe8d0285bd571e3374dd7a
RRUZ <Rodrigo.Ruz.V@gmail.com>
2020-07-29 01:43:26 -0400

    removed auto-generated res files removed old build scripts Improved source formatting for   Vcl.Styles.Ext   Vcl.Styles.NC
    Updated copyright notice
    removed warnings for 10.4

--- Demos/Vcl Styles Utils NC (Demo App)/uAlphaGradient.pas
+++ Demos/Vcl Styles Utils NC (Demo App)/uAlphaGradient.pas
@@ -4,8 +4,7 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Styles.NC, Vcl.ImgList, Vcl.StdCtrls,
-  System.ImageList;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Styles.NC, Vcl.ImgList, Vcl.StdCtrls;
 
 type
   TFrmAlphaGradient = class(TForm)
