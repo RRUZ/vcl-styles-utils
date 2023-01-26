ca182086417c09377574b5d046dd0c15fb61cb65
RRUZ <Rodrigo.Ruz.V@gmail.com>
2023-01-26 16:46:41 -0300

    * Updated  Vcl.Styles.Ext.TSourceInfo for RS 11.2  * Fixed TStyleManagerHelper.RegisteredStyles for RS 11.2

--- Common/Vcl.Styles.Ext.pas
+++ Common/Vcl.Styles.Ext.pas
@@ -93,6 +93,7 @@ type
   TSourceInfo = record
     Data: TStyleServicesHandle;
     StyleClass: TCustomStyleServicesClass;
+    {$IF CompilerVersion >= 35}DesigningState: Boolean;{$IFEND}
   end;
 
 {$REGION 'Documentation'}
@@ -411,6 +412,12 @@ begin
 {$ELSE}
   p := Pointer(PByte(@Self.Flags) + 4);
 {$ENDIF CPUX64}
+
+{$IF (CompilerVersion >= 35)}  //Alexandria.
+  with Self do
+    p := Pointer(@FRegisteredStyles);
+{$IFEND}
+
   LRegisteredStyles := TDictionary<string, TSourceInfo>(p^);
 {$IFEND}
   for t in LRegisteredStyles do
