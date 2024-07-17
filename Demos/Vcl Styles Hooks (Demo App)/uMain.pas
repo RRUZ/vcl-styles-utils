43490c59a89a8a4e27774d0abcdff16a77600a9f
WeberAndre <WeberAndre@gmx.de>
2024-07-17 14:05:38 +0200

    Revert "Merge remote-tracking branch 'upstream/master'"
    This reverts commit 37875abcc379031ba23daa302fa4558547075bad, reversing
    changes made to 9a75a2ea337b64a904c49018764f1453f17cd68b.

--- Demos/Vcl Styles Hooks (Demo App)/uMain.pas
+++ Demos/Vcl Styles Hooks (Demo App)/uMain.pas
@@ -5,7 +5,7 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ComCtrls, Vcl.ImgList,
-  Vcl.ExtCtrls, Vcl.Mask;
+  Vcl.ExtCtrls, Vcl.Mask, System.ImageList;
 
 type
   TFrmMain = class(TForm)
